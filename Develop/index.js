3f930f00dd4ed33a634c23f1cd8340de8fd26262
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2020-10-23 19:12:14 -0400

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,48 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "Enter the title of your project."
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Enter a description of your repository. "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Enter instructions required to install your project."
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Provide instructions and examples for use."
+        },
+        {
+            type: "input",
+            name: "credits",
+            message: "List out your collaborators/third-party assets/tutrorials used in making your repository."
+        },
+        {
+            type: "input",
+            name: "License",
+            message: "Enter a license for your repository."
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your github username."
+        },
+    ]);
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
