96ba9f7bf8751578802a9f3af942a4624ba2d151
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2020-10-25 10:15:14 -0400

    questions working

--- Develop/index.js
+++ Develop/index.js
@@ -2,56 +2,71 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
-const writeFileAsync = util.promisify(fs.writeFile);
+const fileName = README.md
 
-function promptUser() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "title",
-            message: "Enter the title of your project."
-        },
-        {
-            type: "input",
-            name: "description",
-            message: "Enter a description of your repository. "
-        },
-        {
-            type: "input",
-            name: "installation",
-            message: "Enter instructions required to install your project."
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: "Provide instructions and examples for use."
-        },
-        {
-            type: "input",
-            name: "credits",
-            message: "List out your collaborators/third-party assets/tutrorials used in making your repository."
-        },
-        {
-            type: "input",
-            name: "License",
-            message: "Enter a license for your repository."
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "Enter your github username."
-        },
-    ]);
-}
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Enter the title of your project."
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a description of your repository. "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Enter instructions required to install your project."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use."
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "List out your collaborators/third-party assets/tutrorials used in making your repository."
+    },
+    {
+        type: "input",
+        name: "License",
+        message: "Enter a license for your repository."
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your github username."
+    },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("YAY, your README was generated")
+    });
 }
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // function to initialize program
-function init() {
+async function init() {
+    try {
 
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Successfully wrote to README.md");
+
+    } catch (err) {
+        console.log(err);
+    }
 }
+
 
 // function call to initialize program
 init();
